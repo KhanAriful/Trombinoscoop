c44f571e06744148588db57d6e5b1e983f68bcfd
Ariful Khan <ank4219@gmail.com>
2021-06-15 00:20:58 +0200

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import PublicRoutes from './routes/publicRoutes'
+import PrivateRoutes from './routes/privateRoutes'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Demo
+  const isLoggedIn = false
+
+  return isLoggedIn
+  ? <PrivateRoutes />
+  : <PublicRoutes />
 }
 
 export default App;
